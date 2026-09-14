70dcb79507a977f00a2928219bbd98bf9bd70c03
Ashish Rokade <ashishrokade944@gmail.com>
2026-09-14 22:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your code here
+function processImage(){
+	const file = documen.getElementById("imageInput").files[0];
+	if(!file){
+		return;
+	}
+
+	const originalImage = document.getElementById("originalImage");
+	const processedImage = document.getElementById("processedImage");
+
+	const imageURL = URL.createdObjectUrl(file);
+	originalImage.src = imageURL;
+	const formData = new FormData();
+	formData.append("image", file);
+
+
+
+	fetch("https://api.imagga.com/v2/colors", {
+		method: "POST",
+		body: formData,
+		headers: {
+			"Authorization": "Basic" + btoa("api_key:api_secret")
+		}
+	})
+	.then(response => response.json())
+	.then(data => {
+		console.log(data);
+		processedImage.src = imageURL;
+	})
+	.catch(error => {
+		console.error(error);
+	});
+}
+
+
+
+
+
+
+
+
+
+
